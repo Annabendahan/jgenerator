52f90554b7521be947945387de2380d2b8be96df
Annabendahan <an.bendahan@gmail.com>
2021-06-28 07:41:01 +0200

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `JERSEY'S GENERATOR`,
-    description: `Jersey generator`,
+    description: `Jersey's generator`,
     author: `Anna bdh `,
   },
   plugins: [
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/jersey.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
